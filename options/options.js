3225a6f0a646cfe391ad9742fe8e4d1b3506dec7
Crowdbotics <team@crowdbotics.com>
2023-06-27 13:12:31 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "nenad-za-koricu-41756",
+  url: "https://nenad-za-koricu-41756.botics.co",
+  api: "https://nenad-za-koricu-41756.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
